7574c4483363b5c229d782a7d2b818e1f00c67fc
shakhera <shakherakhanom024@gmail.com>
2025-02-01 02:00:58 +0600

    HeroSection update

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ function App() {
       <div className="container mx-auto p-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </div>
     </div>
--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -2,21 +2,24 @@ import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   return (
-    <section className="py-12 h-[435px]">
-      <aside className="relative flex flex-col lg:flex-row items-centerspace-x-3">
+    <section className="pb-10 h-auto lg:h-[435px]">
+      <aside className="relative flex flex-col lg:flex-row items-center">
         <div className="container mx-auto flex flex-col lg:flex-row items-center">
-          <div className="lg:w-1/2">
+          <div className="w-full lg:w-1/2">
             <img
               src="https://img.freepik.com/free-photo/foundation-bottles-advertising-arrangement_23-2149511225.jpg?semt=ais_hybrid"
               alt="Face Skincare"
-              className="w-full object-cover h-full"
+              className="w-full h-64 lg:h-full object-cover"
             />
           </div>
-          <div className="relative text-left text-black z-10 lg:w-1/2 px-20">
+
+          <div className="relative text-center text-black z-10 w-full lg:w-1/2 px-4 lg:px-20 py-8 lg:py-0">
             <p className="text-muted-foreground font-bold mb-5">
               Organic Collection
             </p>
-            <h3 className="mb-5 text-5xl font-bold ">Face Skincare</h3>
+            <h3 className="mb-5 text-3xl lg:text-5xl font-bold">
+              Face Skincare
+            </h3>
             <p className="text-muted-foreground font-bold mb-5">
               Elevate Your Basics with a Touch of Glan
             </p>
